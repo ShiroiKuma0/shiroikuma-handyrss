397940b4a72f94816587404cb58e371256f96ff3
白い熊 <ShiroiKuma@sumo.do>
2026-05-24 14:31:21 +0200

    Chrome colors D2a: runtime window/nav background (chrome_bg picker)

--- FlymFork/src/main/java/ru/yanus171/feedexfork/utils/Theme.java
+++ FlymFork/src/main/java/ru/yanus171/feedexfork/utils/Theme.java
@@ -42,6 +42,7 @@ public class Theme {
 	public static final String NEW_ARTICLE_INDICATOR_RES_ID = "NEW_ARTICLE_INDICATOR";
 	public static final String STARRED_ARTICLE_INDICATOR_RES_ID = "STARRED_ARTICLE_INDICATOR";
 	private static final String TOOL_BAR_COLOR = "toolBarColor";
+	public static final String CHROME_BG = "chrome_bg";
 	private static HashMap<String, HashMap<String, String>> ThemeList = null;
 	static final String THEME_CUSTOM = "Custom";
 	private static String mTheme = DARK;
@@ -195,6 +196,7 @@ public class Theme {
 		return Theme.GetResID(STYLE_THEME );
 	}
 	//-------------------------------------------------------------------
+	public static int GetChromeBgInt() { return Color.parseColor( PrefUtils.getString( CHROME_BG, "#000000" ) ); }
 	public static int GetColorInt(String key, int defID) {
 		int result = Color.BLACK;
 		try {
